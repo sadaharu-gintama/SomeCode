9d699894e2132e69bc4ceb31ad393c099048c29b
SaDaHaLu <yi.liu.197@gmail.com>
2015-12-19 23:12:13 -0700

    Calculate Correlation of different assets

---
+++ correlation.py
@@ -0,0 +1,22 @@
+from yahoo_finance import Share
+#import numpy as np
+import pandas as pd
+from pandas import DataFrame
+#from matplotlib import pyplot as plt
+
+start_date = '2000-01-01'
+end_date = '2015-01-01'
+
+sticker = ['SPY','ASHR','AGG','GLD','DSUM','HEWG','EWJ','XLU', 'XLE']
+stocks = [Share(x) for x in sticker]
+rtns = DataFrame()
+for stock in stocks:
+    data = stock.get_historical(start_date, end_date)
+    price = [float(x['Close']) for x in data]
+    name = stock.symbol
+    df = DataFrame(price, columns=[name])
+    df[name][1:] = df[name][1:].values / df[name][:-1].values - 1  
+    df[name][:1] = 0
+    rtns = pd.concat([rtns,df], axis=1, join='inner')
+
+rtns.corr().to_csv("Data.csv")
