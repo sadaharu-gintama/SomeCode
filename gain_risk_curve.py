c2313861eea87411aa31576a74fcb6043cbfa101
SaDaHaLu <yi.liu.197@gmail.com>
2015-12-22 22:37:30 -0700

    change the plot

--- gain_risk_curve.py
+++ gain_risk_curve.py
@@ -4,8 +4,8 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 import matplotlib.dates as Dates
 
-start_date = '2014-01-01'
-end_date = '2015-01-01'
+start_date = '2015-01-01'
+end_date = '2015-12-01'
 # SCHB: Schwab U.S. Broad Market ETF
 # SCHA: Schwab U.S. Small Cap ETF
 # SCHH: Schwab U.S. REITS ETF
@@ -14,12 +14,14 @@ end_date = '2015-01-01'
 # SCHZ: Schwab U.S. Aggregate Bond ETF
 # SCHP: Schwab U.S. Tips ETF
 # DSUM: PowerShare Chinese Yuan Dim Sum Bond ETF
-sticker = ['SPY', 'SCHZ', 'DSUM']#,'SCHA','SCHH','SCHF','SCHE','SCHZ','SCHP','DSUM']
+sticker = ['SCHB', 'SCHZ', 'DSUM']#,'SCHA','SCHH','SCHF','SCHE','SCHZ','SCHP','DSUM']
 stocks = [Share(x) for x in sticker]
 
 for stock in stocks:
     data = stock.get_historical(start_date, end_date)
-    price = [float(x['Close'])*10.0 for x in data]
+    price = [float(x['Close']) for x in data]
     date = [Dates.datestr2num(x['Date']) for x in data]
+    plt.subplot(3,1,stocks.index(stock) + 1)
     plt.plot_date(date, price, 'r-')
-    plt.show()
+
+plt.show()
