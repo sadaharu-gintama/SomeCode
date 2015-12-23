520d1d407707059de74611f3ca11975ea18467f3
SaDaHaLu <yi.liu.197@gmail.com>
2015-12-22 17:44:30 -0700

    curve of value vs date

---
+++ gain_risk_curve.py
@@ -0,0 +1,25 @@
+from yahoo_finance import Share
+import pandas as pd
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import matplotlib.dates as Dates
+
+start_date = '2014-01-01'
+end_date = '2015-01-01'
+# SCHB: Schwab U.S. Broad Market ETF
+# SCHA: Schwab U.S. Small Cap ETF
+# SCHH: Schwab U.S. REITS ETF
+# SCHF: Schwab International Equity ETF
+# SCHE: Schwab Emerging Market ETF
+# SCHZ: Schwab U.S. Aggregate Bond ETF
+# SCHP: Schwab U.S. Tips ETF
+# DSUM: PowerShare Chinese Yuan Dim Sum Bond ETF
+sticker = ['SPY', 'SCHZ', 'DSUM']#,'SCHA','SCHH','SCHF','SCHE','SCHZ','SCHP','DSUM']
+stocks = [Share(x) for x in sticker]
+
+for stock in stocks:
+    data = stock.get_historical(start_date, end_date)
+    price = [float(x['Close'])*10.0 for x in data]
+    date = [Dates.datestr2num(x['Date']) for x in data]
+    plt.plot_date(date, price, 'r-')
+    plt.show()
