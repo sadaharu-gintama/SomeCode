aa50db560fe707c08c20073655bab481efc4956f
Yi Liu <yi.liu.197@gmail.com>
2016-06-05 21:03:11 -0700

    mathematical tools finance

--- Stock/correlation.py
+++ Stock/correlation.py
@@ -20,7 +20,7 @@ for stock in stocks:
     price = [float(x['Close']) for x in data]
     name = stock.symbol
     df = DataFrame(price, columns=[name])
-    df[name][1:] = df[name][1:].values / df[name][:-1].values - 1  
+    df[name][1:] = df[name][1:].values / df[name][:-1].values - 1
     df[name][:1] = 0
     rtns = pd.concat([rtns,df], axis=1, join='inner')
 
